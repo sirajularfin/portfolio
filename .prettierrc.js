d984f0c64f51361a72df8f81c9f556c563a287ce
Sirajul Arfin <sirajularfin@gmail.com>
2024-10-26 00:09:25 +0530

    Add prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // Use semicolons at the end of statements
+  semi: true,
+
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+  jsxSingleQuote: true,
+
+  // Control spacing inside object literals
+  bracketSpacing: true,
+  bracketSameLine: false,
+
+  // Set the number of spaces per indentation level
+  tabWidth: 2,
+
+  // Print trailing commas wherever possible
+  trailingComma: 'all',
+
+  // Preserve quotes on object properties
+  quoteProps: 'preserve',
+
+  // Maintain existing line endings
+  endOfLine: 'auto',
+
+  // Avoid parentheses around a sole arrow function parameter
+  arrowParens: 'avoid',
+
+  // Specify the line length that the printer will wrap on
+  printWidth: 100,
+};
