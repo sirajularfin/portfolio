9ff2f2f4130841a336e5b361917a67ce59e1b788
Sirajul Arfin <sirajularfin@gmail.com>
2024-10-26 14:16:56 +0530

    Update theme files

---
+++ src/theme/colors.ts
@@ -0,0 +1,18 @@
+export enum Colors {
+  // Blue Grey Palette
+  BLUE_GREY_50 = '#ECEFF1',
+  BLUE_GREY_900 = '#212121',
+
+  // Grey Palette
+  GREY_400 = '#BDBDBD',
+  GREY_700 = '#616161',
+  GREY_800 = '#424242',
+
+  // Indigo Palette
+  INDIGO_A700 = '#304FFE',
+
+  // Others
+  WHITE = '#FFFFFF',
+  BLACK = '#000000',
+  TRANSPARENT = 'TRANSPARENT',
+}
--- src/theme/sizes.ts
+++ src/theme/sizes.ts
@@ -2,5 +2,8 @@ export enum ScaledSize {
 	AUTO = 'auto',
 	SCREEN_WIDTH = '100vw',
 	SCREEN_HEIGHT = '100vh',
+	SIZE_1 = '1px',
+	SIZE_10 = '10px',
+	SIZE_20 = '20px',
 	ZERO = '0px',
 }
